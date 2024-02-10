40c176e1263281ccf555540a360984244f4023fc
Young Lmao <artomushakov@mail.ru>
2024-02-10 21:36:47 +0300

    а

--- Program.cs
+++ Program.cs
@@ -20,44 +20,6 @@ namespace MainProgram
         {
             GameWindow window = new GameWindow(1200,800);
             window.OpenWindow();
-
-
-
-
-
-            //RenderWindow window = new RenderWindow(new VideoMode(1920, 1050), "Tank");
-            //window.Closed += (sender, e) => { window.Close(); };
-            //window.KeyPressed += KeyClick;
-            //SFML.System.Clock clock = new Clock();
-            //float deltaTime = 0.0f;
-            //float frameTime;
-            //Game game = new Game();
-            //game.LoadLevel();
-            //while(window.IsOpen)
-            //{
-            //    window.DispatchEvents();
-
-
-
-
-            //    frameTime = clock.Restart().AsSeconds();
-            //    deltaTime += frameTime;
-            //    while (deltaTime > 0.016f)
-            //    {
-            //        window.Clear(Color.Black);
-            //        if (game.GetGameStatus()) game.show(window);
-            //        //if (menu.MenuActive()) menu.action_show(window);
-            //        deltaTime -= 0.016f;
-            //        window.Display();
-            //    }
-            //}
-            
         }
-        //void EventHandling(in RenderWindow window)
-        //{
-        //    Event e;
-        //    while(window.PollEvent(e))
-
-        //}
     }
 }
